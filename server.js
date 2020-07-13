90e9498b502c0b52644108f2de34d7ee13d23589
dpak1999 <deepakkumardash313@gmail.com>
2020-07-13 19:48:35 +0530

    configuring env variables for prod and dev

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 /*IMPORTING PACKAGES */
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
+// console.log(process.env);
+
 /*SERVER SETUP */
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server up and running at port ${PORT}`);
 });
